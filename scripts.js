a1d48bd9dd3977af6d8c9cc6c51bca07b8ca630c
Carlos Henrique <fariascarlos2346@gmail.com>
2025-10-13 19:03:24 -0300

    finalizado projeto

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 const form = document.getElementById("converterForm")
 const amount = document.getElementById("amount")
 const fromCurrency = document.getElementById("fromCurrency")
-const convertedAmount = document.getElementById("convertedAmount")
 const toCurrency = document.getElementById("toCurrency")
+const convertedAmount = document.getElementById("convertedAmount")
 const loading = document.querySelector(".loading")
 const result = document.querySelector(".result")
 const error = document.querySelector(".error")
@@ -12,31 +12,38 @@ const API_URL = "https://api.exchangerate-api.com/v4/latest/"
 async function convertMoney(){
 
     loading.style.display = "block"
+    error.style.display = "none"
+    result.style.display = "none"
 
     try {
-        const response = await fetch(API_URL + fromCurrency.value)
-        const data = await response.json()
-        
-        const rate = data.rates[toCurrency.value]
-        const convertedValue = (amount.value * rate).toFixed(2)
+       const response = await fetch(API_URL + fromCurrency.value)
+       const data = await response.json()
+       
+       const rate = data.rates[toCurrency.value]
+       const convertedValue = (amount.value * rate).toFixed(2)
 
-        convertedAmount.value = convertedValue
+       convertedAmount.value = convertedValue
+       result.style.display = "block"
 
-        result.innerHTML = `
+       result.innerHTML = `
+        <div style="font-size: 1.4rem; margin-top: 10px;">
+            ${amount.value} ${fromCurrency.value} = ${convertedAmount.value} ${toCurrency.value}
+        </div>
+        <div style="font-size: 0.9rem; opacity: 0.8;">
+            Taxa: 1 ${fromCurrency.value} = ${rate} ${toCurrency.value}
+        </div>
 
-            <div>
-                ${amount.value} ${fromCurrency.value} = ${convertedAmount.value} ${toCurrency.value}
-            </div>
-            <div>
-                Taxa: 1 ${fromCurrency.value} = ${rate} ${toCurrency.value}
-            </div>
-
-        `
-
+       
+       `
+       
     }
-    catch(error){
-        alert("Falha no servidor")
+    catch(err){
+        console.error(err)
+        error.style.display = "block"
+        error.innerHTML = `Falha ao converter moeda! Tenta novamente.`
     }
+
+    loading.style.display = "none"
     
 }
 
@@ -45,3 +52,4 @@ form.addEventListener("submit", function(event){
     event.preventDefault()
     convertMoney()
 })
+
